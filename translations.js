f7c5b1ee30a3975aafff58540635ac17ef2cf478
sesgarites <montsetarrus71@gmail.com>
2026-03-21 08:17:15 -0400

    Create translations.js file with English and Spanish translations

---
+++ translations.js
@@ -0,0 +1,12 @@
+const translations = {
+  en: {
+    welcome: "Welcome",
+    goodbye: "Goodbye"
+  },
+  es: {
+    welcome: "Bienvenido",
+    goodbye: "Adiós"
+  }
+};
+
+export default translations;
